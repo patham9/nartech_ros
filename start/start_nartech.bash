6829a4eef19e3504bc47f7c67393696395e1113e
Patrick Hammer <patham9@gmail.com>
2024-10-29 15:09:35 +0100

    Update: updated start file for lousy but better graphics accel

--- start/start_nartech.bash
+++ start/start_nartech.bash
@@ -1,2 +1,3 @@
 export QT_QPA_PLATFORM=xcb
-ros2 launch nav2_bringup tb4_simulation_launch.py slam:=True nav:=True headless:=False autostart:=True rviz_config_file:=nartech_view.rviz
+export LIBGL_ALWAYS_SOFTWARE=1
+ros2 launch nav2_bringup tb4_simulation_launch.py slam:=True nav:=True headless:=False autostart:=True use_sim_time:=True rviz_config_file:=nartech_view.rviz
