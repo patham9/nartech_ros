ea448b3da6ca5760e50fa21fbd40475a57519635
Peter <pisaev1@gmail.com>
2024-11-19 13:19:03 -0500

    changed update_updater into update_wrapper

--- start/update_updater.bash
+++
@@ -1,7 +0,0 @@
-#Called at startup to keep nartech up-to-date!
-#1. we pull to ensure nartech_ws to ensure we have the newest update script
-cd ~/nartech_ws/src/nartech_ros/
-git pull
-#2. we execute the update script
-cd ~/nartech_ws/src/nartech_ros/start/
-./update_nartech.bash
