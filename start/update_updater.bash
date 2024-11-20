3873384c8aad9331c4f3876549599464427f47cd
Peter <pisaev1@gmail.com>
2024-11-20 09:47:18 -0500

    new updater scripts

---
+++ start/update_updater.bash
@@ -0,0 +1,12 @@
+# Called at startup to pull updater script
+# and invoke it to update the workspace 
+# i.e. pull src and build ROS workspace
+zenity --question --text="Do you wish to update NARTECH workspace?"
+if [ $? == "0" ]; then 
+    cd /home/nartech/nartech_ws/src/nartech_ros/start
+    rm update_nartech.bash
+    wget https://raw.githubusercontent.com/patham9/nartech_ros/refs/heads/master/start/update_nartech.bash
+    chmod 775 update_nartech.bash
+    ./update_nartech.bash
+fi
+
--- start/update_wrapper.bash
+++
@@ -1,9 +0,0 @@
-# Called at startup to pull updater script
-# and invoke it to update the workspace 
-# i.e. pull src and build ROS workspace
-cd /home/nartech/nartech_ws/src/nartech_ros/start
-rm update_nartech.bash
-wget https://raw.githubusercontent.com/patham9/nartech_ros/refs/heads/master/start/update_nartech.bash
-chmod 775 update_nartech.bash
-./update_nartech.bash
-
