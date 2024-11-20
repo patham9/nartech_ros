e50058c6eedfae091742eee2c2fa368b6cdee644
Peter <pisaev1@gmail.com>
2024-11-20 09:48:40 -0500

    added update_updater and update_nartech

--- start/update_nartech.bash
+++ start/update_nartech.bash
@@ -1,23 +1,37 @@
+# This updater script pull the src code for
+# NACE, ONA, MeTTa-NARS, MeTTa-Morph, NARS-GPT
+# as well as builds packages for ROS workspace
+
 cd ~/nartech_ws/src/nartech_ros/
 git pull
+
 cd ~/nartech_ws
 colcon build
+
 cd ~/AniNAL
 git pull
 sh ./build.sh
+
 cd ~/OpenNARS-for-Applications
 git pull
 #./build.sh #uncomment if it should build it every startup (not needed by NARtech demos currently)
+
 cd ~/NACE
 git pull
+
 cd ~/metta-morph
 git pull
+
 cd ~/metta-morph/metta-nars
 git pull
+
 #metta-nars in metta-morph automatically re-compiles at first run when code changes
 cd ~/metta-nars
 git pull
+
 cd ~/NARS-GPT
 git pull
 #./build.sh #uncomment if it should build it every startup (not needed by NARtech demos currently)
-xmessage -center "NARtech up-to-date!"
+
+zenity --info --text "  NARTECH workspace is up-to-date!  " --title="Update Status" --icon="/home/nartech/NARTECH/graphics/logo.png"
+
