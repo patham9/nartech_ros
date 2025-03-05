b3cdb3e5bbbdb06816bbacc641fbfd580914f3ff
Patrick Hammer <patham9@gmail.com>
2025-03-05 15:51:37 +0100

    Update: mettabridge can now also invoke NARS

--- channels/narsbridge.py
+++ channels/narsbridge.py
@@ -18,13 +18,47 @@ def extract_metta_values(d):
     if isinstance(d, dict):
         for key, value in d.items():
             if key == 'metta':
-                values.append(value)
+                values.append(value.replace("#","$"))
             else:
                 values.extend(extract_metta_values(value))
     elif isinstance(d, list):  # Handle lists of dictionaries
         for item in d:
             values.extend(extract_metta_values(item))
     return values
+
+#helper function for debugging
+def exit42(s="42"):
+    return SExprParser(s).parse(runner.tokenizer())
+
+def call_narsIO(*a):
+    global runner
+    cmd = str(a[0])
+    unknownCommand = True
+    threshold = ""
+    for narscommand in ["AddBeliefEvent", "AddBeliefEternal", "AddGoalEvent",
+                        "EventQuestion", "EternalQuestion", "(EternalQuestionMultiple", "(EventQuestionMultiple"]:
+        if cmd.startswith(f"({narscommand} "):
+            if narscommand == "(EternalQuestionMultiple":
+                threshold = cmd.split("(EternalQuestionMultiple ")[1].split(" ")[0]
+            ret = NAR_AddInput(f"!"+cmd)
+            narsret = "("
+            for category in ['input', 'derivations', 'answers']:
+                renamed = {'input': "INPUT", "derivations": "DERIVATION", "answers": "ANSWER"}
+                if category in ret:
+                    for s in ret[category]:
+                        if 'metta' in s:
+                            addition = f"({renamed[category]} " + s['metta'] + ')'
+                            opening_parantheses = len([x for x in addition if x=="("])
+                            closing_parantheses = len([x for x in addition if x==")"])
+                            if "$" not in addition and "#" not in addition: #TODO investigate
+                                narsret += addition
+            narsret += ")"
+            parser = SExprParser(narsret)
+            unknownCommand = False
+            break
+    if unknownCommand:
+        parser = SExprParser(f"(Unknown command: {cmd})")
+    return parser.parse(runner.tokenizer())
 
 def call_narsinput(*a):
     global runner
@@ -34,8 +68,7 @@ def call_narsinput(*a):
     for narscommand in ["AddBeliefEvent", "AddBeliefEternal", "AddGoalEvent", "EventQuestion", "EternalQuestion"]:
         if cmd.startswith(f"({narscommand} "):
             ret = NAR_AddInput(f"!({narscommand} "+cmd.split(f"({narscommand} ")[1])
-            narsret = "(" + (" ".join(extract_metta_values(ret))) + ")"
-            parser = SExprParser(narsret)
+            parser = SExprParser("()")
             unknownCommand = False
             break
     if unknownCommand:
